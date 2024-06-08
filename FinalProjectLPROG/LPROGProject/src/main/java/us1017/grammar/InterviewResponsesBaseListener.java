2870fe9ade4e07d10cf3bf1cb9d640311f668854
1232233 <1232233@isep.ipp.pt>
2024-06-08 12:31:22 +0100

    added files manual
    via browser

---
+++ FinalProjectLPROG/LPROGProject/src/main/java/us1017/grammar/InterviewResponsesBaseListener.java
@@ -0,0 +1,136 @@
+// Generated from C:/Users/jerem/OneDrive - bwedu/Studium/7tes Semester/LPROG/05_SprintC/LPROG/FinalProjectLPROG/LPROGProject/src/main/java/us1017/grammar/InterviewResponses.g4 by ANTLR 4.13.1
+package us1017.grammar;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link InterviewResponsesListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class InterviewResponsesBaseListener implements InterviewResponsesListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterPlugin(InterviewResponsesParser.PluginContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitPlugin(InterviewResponsesParser.PluginContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterInterviewParams(InterviewResponsesParser.InterviewParamsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitInterviewParams(InterviewResponsesParser.InterviewParamsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterQuestionList(InterviewResponsesParser.QuestionListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitQuestionList(InterviewResponsesParser.QuestionListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterQuestionResponse(InterviewResponsesParser.QuestionResponseContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitQuestionResponse(InterviewResponsesParser.QuestionResponseContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterQuestionType(InterviewResponsesParser.QuestionTypeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitQuestionType(InterviewResponsesParser.QuestionTypeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterResponse(InterviewResponsesParser.ResponseContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitResponse(InterviewResponsesParser.ResponseContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDate(InterviewResponsesParser.DateContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDate(InterviewResponsesParser.DateContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTime(InterviewResponsesParser.TimeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTime(InterviewResponsesParser.TimeContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ FinalProjectLPROG/LPROGProject/src/main/java/us1017/grammar/InterviewResponsesVisitor.java
@@ -0,0 +1,61 @@
+// Generated from C:/Users/jerem/OneDrive - bwedu/Studium/7tes Semester/LPROG/05_SprintC/LPROG/FinalProjectLPROG/LPROGProject/src/main/java/us1017/grammar/InterviewResponses.g4 by ANTLR 4.13.1
+package us1017.grammar;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link InterviewResponsesParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface InterviewResponsesVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link InterviewResponsesParser#plugin}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPlugin(InterviewResponsesParser.PluginContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link InterviewResponsesParser#interviewParams}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitInterviewParams(InterviewResponsesParser.InterviewParamsContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link InterviewResponsesParser#questionList}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitQuestionList(InterviewResponsesParser.QuestionListContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link InterviewResponsesParser#questionResponse}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitQuestionResponse(InterviewResponsesParser.QuestionResponseContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link InterviewResponsesParser#questionType}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitQuestionType(InterviewResponsesParser.QuestionTypeContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link InterviewResponsesParser#response}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitResponse(InterviewResponsesParser.ResponseContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link InterviewResponsesParser#date}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDate(InterviewResponsesParser.DateContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link InterviewResponsesParser#time}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTime(InterviewResponsesParser.TimeContext ctx);
+}
